d0e2c4888d806a9a957d229065d59efd2e20233b
sunlei <839228534@qq.com>
2019-11-10 21:26:15 +0800

    add require-directory

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const Koa = require('koa')
+const requireDirectory = require('require-directory')
+const Router = require('koa-router')
+
+const router = new Router()
+
+router.get('/',(ctx,next) => {
+
+})
+
 
 const app = new Koa()
 
-app.use(async (ctx)=>{
-  ctx.body = "hello"
-})
+
 
 app.listen(8000,()=>{
   console.log('server is running in 8000')
